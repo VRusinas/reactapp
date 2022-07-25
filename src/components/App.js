2d13199a995f13f434a3e645f543073bdcda25f9
ViliusRusinas <vilius.rusinas@stud.vilniustech.lt>
2022-07-25 13:34:39 +0300

    commit to 'master'

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import NavBar from './NavigationBar/NavBar'
+import TextConatiner from './TextContainer/TextContainer';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <TextConatiner/>
+      <TextConatiner/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavigationBar/NavBar.js
@@ -0,0 +1,13 @@
+import './NavBar.css'
+
+const SearchBar = () => {
+    return (
+        <div class="navbar">
+            <a href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+        </div>
+
+    )
+}
+export default SearchBar
